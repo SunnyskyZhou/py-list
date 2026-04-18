045d44fcc233b8e0b61fe039038601775b1aaa4a
SunnyskyZhou <152792435+SunnyskyZhou@users.noreply.github.com>
2026-04-18 09:55:54 +0800

    Add files via upload

---
+++ print训练/main.py
@@ -0,0 +1,24 @@
+#print("HELLO " * 30)
+
+#print(10>5>3)
+#print(1>5>3)
+#print(not 10>5>3)
+
+'''print('hello world!')
+if 3 > 0:
+    print('OK')
+    print('yes')
+
+username = input('username: ')
+print('welcome', username)
+print('welcome ' + username)'''
+
+
+
+
+
+
+
+
+
+
